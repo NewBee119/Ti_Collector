bd1ed51067f9716e38478fdc9802b44f5bda643c
scu-igroup <qswhs@foxmail.com>
2017-10-26 14:33:53 +0800

    Update 37.py

--- BlackListSpider/script/37.py
+++ BlackListSpider/script/37.py
@@ -9,4 +9,4 @@ import download
 stamp  ='reputation'
 source = '37'
 url    = 'https://reputation.alienvault.com/reputation.generic'
-download.download_ip(source, stamp, url)
+download.download_ip_2(source, stamp, url)
