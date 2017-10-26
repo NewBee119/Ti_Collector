77d327200fb2e19b172bcf20bc0f84ea76354231
scu-igroup <qswhs@foxmail.com>
2017-10-26 15:59:19 +0800

    delete some files

--- BlackListSpider/script/34.py
+++
@@ -1,7 +0,0 @@
-#encoding=utf8
-
-import download
-source ='34'
-stamp  ='badips' 
-url    ='https://zeustracker.abuse.ch/blocklist.php?download=badips'
-download.download_ip(source, stamp, url)
--- BlackListSpider/script/7.py
+++
@@ -1,6 +0,0 @@
-import download
-
-url = 'https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/botscout_1d.ipset'
-source =  '7'
-stamp = 'abuse'
-download.download_ip(source,stamp,url)
