bccba5da6e410daf3bd013760b969d30c393862f
scu-igroup <qswhs@foxmail.com>
2017-10-26 14:35:20 +0800

    Update 44.py

--- BlackListSpider/script/44.py
+++ BlackListSpider/script/44.py
@@ -9,4 +9,4 @@ import download
 source = '44'
 stamp  = 'BlackList'
 url    = 'http://www.joewein.net/dl/bl/dom-bl.txt'
-download.download_ip(source, stamp, url)
+download.download_domain(source, stamp, url)
