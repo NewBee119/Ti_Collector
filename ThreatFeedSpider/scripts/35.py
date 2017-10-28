adce11db8637b31411ebffb71e944bbbc56bc489
scu-igroup <qswhs@foxmail.com>
2017-10-28 15:21:35 +0800

    Update 35.py

--- ThreatFeedSpider/scripts/35.py
+++ ThreatFeedSpider/scripts/35.py
@@ -1,12 +1,12 @@
 # encoding=utf8
 #date:2017-10-19
 #domain type
-#source:https://reputation.alienvault.com/reputation.generic
-#source_id:37
+#source:https://zeustracker.abuse.ch/blocklist.php?download=compromised
+#source_id:35
 
 import download
 
 stamp  = 'BlackList'
-source = '37'
-url    = 'https://reputation.alienvault.com/reputation.generic'
+source = '35'
+url    = 'https://zeustracker.abuse.ch/blocklist.php?download=compromised'
 download.download_ip_2(source, stamp, url)
